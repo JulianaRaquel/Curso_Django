da96a9f96df21709148f3b552bc4a5c75a8ceca6
Juliana Raquel <julianamedeiros_228@hotmail.com>
2022-06-28 18:11:10 -0300

    quintocommit

--- pypro32/settings.py
+++ pypro32/settings.py
@@ -103,7 +103,6 @@ STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles'),
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR / 'mediafiles'),
 
-COLLECTFAST_ENABLE = False
 
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', default=False)
 # Configuração para o S3
@@ -116,9 +115,6 @@ if AWS_ACCESS_KEY_ID:
     AWS_QUERYSTRING_AUTH = True
     AWS_S3_CUSTOM_DOMAIN = None
     AWS_DEFAULT_ACL = 'private'
-
-    COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
-    COLLECTFAST_ENABLE = True
 
     # configurações dos arquivos estáticos
     STATICFILES_STORAGE = 's3-folder-storage.s3.StaticStorage'
