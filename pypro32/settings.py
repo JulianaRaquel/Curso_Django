9a03feebc8dfcaca42fafc77d33d56c111a7270b
Juliana Raquel <julianamedeiros_228@hotmail.com>
2022-06-29 19:17:54 -0300

    modificação14

--- pypro32/settings.py
+++ pypro32/settings.py
@@ -122,14 +122,14 @@ if AWS_ACCESS_KEY_ID:
     COLLECTFAST_ENABLED = True
 
     # configurações dos arquivos estáticos
-    STATICFILES_STORAGE = 's3-folder-storage.s3.StaticStorage'
+    STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
     STATIC_S3_PATH = 'static'
     STATIC_ROOT = f'/{STATIC_S3_PATH}/'
     STATIC_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{STATIC_S3_PATH}/'
     ADMIN_MEDIA_PREFIX = STATIC_URL + '/admin'
 
     # configurações dos arquivos de upload
-    DEFAULT_FILE_STORAGE = 's3-folder-storage.s3.StaticStorage'
+    DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.StaticStorage'
     DEFAULT_S3_PATH = 'media'
     MEDIA_ROOT = f'/{DEFAULT_S3_PATH}/'
     MEDIA_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{DEFAULT_S3_PATH}/'
